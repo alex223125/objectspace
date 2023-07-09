a8d1273a4c37f70506e62a0e044f31ec4c61d160
Alexey Sologub <alexeysologub51@gmail.com>
2023-07-08 21:08:38 -0400

    search features and concerns added to framework

--- app/models/frameworks/framework.rb
+++ app/models/frameworks/framework.rb
@@ -1,6 +1,16 @@
 class Frameworks::Framework < ApplicationRecord
 
-  searchkick callbacks: :async, text_middle: [:title, :description]
+  extend FriendlyId
+  friendly_id :slug_candidates, use: [:slugged, :finders, :history]
+
+  extend Pagy::Searchkick
+  searchkick callbacks: :async,
+             text_middle: [:title, :description],
+             word: [:list_of_tags, :ownerable_id]
+  scope :search_import, -> { includes(:tags) }
+  acts_as_taggable_on :tags
+
+  belongs_to :ownerable, polymorphic: true
 
   belongs_to :folder, class_name: "Folder"
 
@@ -9,5 +19,35 @@ class Frameworks::Framework < ApplicationRecord
 
   has_many :class_containers, class_name: "SimpleClasses::ClassContainer", dependent: :destroy
   accepts_nested_attributes_for :class_containers
+
+  # TODO: move to concern for framework and simple_Class
+  def root_class_container
+    self.class_containers.where(parent_id: nil).first
+  end
+
+  def root_interface_group
+    self.interface_groups.where(parent_id: nil).first
+  end
+
+  def owner
+    self.ownerable
+  end
+
+  def search_data
+    {
+      title: title,
+      description: description,
+      list_of_tags: tag_list,
+      ownerable_id: ownerable_id
+    }
+  end
+
+  private
+
+  def slug_candidates
+    [ :title,
+      [:title, :id]
+    ]
+  end
 
 end
