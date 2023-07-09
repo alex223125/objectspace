b1ebd0fbf2d1de0be0baa08ba5937bb28270870a
Alexey Sologub <alexeysologub51@gmail.com>
2023-07-08 21:28:26 -0400

    a lot of routes changes

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,19 @@ Rails.application.routes.draw do
     get 'edit_tos_agreement', to: 'users/registrations#edit_tos_agreement'
   end
 
+
+  # scope "/users" do
+  #   resources :user_settings, only: [:save]
+  # end
+
+  namespace :users do
+    resources :user_settings, only: [:update]
+    resources :users
+  end
+  get "settings/profile/:username", to: "users/user_settings#profile", as: 'user_settings_profile'
+  get "settings/account/:username", to: "users/user_settings#account", as: 'user_settings_account'
+  get 'users_suggestions', to: 'users/users#suggestions', as: 'users_suggestions'
+
   resources :improvements
   resources :unit_usage_examples
 
@@ -22,8 +35,9 @@ Rails.application.routes.draw do
         get :preview
       end
     end
-    resources :unit_versions
+    resources :unit_versions, except: [:show]
   end
+  get "/:username/methods/:id", to: "unit/unit_versions#show", as: 'unit_version'
 
   namespace :algorithm do
     resources :algorithms do
@@ -31,12 +45,15 @@ Rails.application.routes.draw do
         get :preview
       end
     end
-    resources :algorithm_versions
+    resources :algorithm_versions, except: [:show]
     resources :control_structures
     resources :steps
-    resources :substeps
 
+
+    resources :substeps
   end
+  get "/substep_template", to: "algorithm/steps#substep_template", as: 'substep_template'
+  get "/:username/algorithms/:id", to: "algorithm/algorithm_versions#show", as: 'algorithm_version'
 
   namespace :article do
     resources :articles do
@@ -44,11 +61,12 @@ Rails.application.routes.draw do
         get :preview
       end
     end
-    resources :article_versions
+    resources :article_versions, except: [:show]
   end
+  get "/:username/articles/:id", to: "article/article_versions#show", as: 'article_version'
 
   namespace :simple_class do
-    resources :simple_classes do
+    resources :simple_classes, except: [:show] do
       member do
         get :preview
       end
@@ -57,25 +75,45 @@ Rails.application.routes.draw do
     resources :class_containers
     # resources :interface_members
   end
+  get "/:username/classes/:id", to: "simple_class/simple_classes#show", as: 'simple_class'
 
   namespace :framework do
-    resources :frameworks do
+    resources :frameworks, except: [:show] do
       member do
         get :preview
       end
     end
   end
+  get "/:username/framework/:id", to: "framework/frameworks#show", as: 'framework'
 
-  resources :folders
+  resources :folders, except: [:show]
+  get "/:username/folders/:id", to: "folders#show", as: 'target_folder'
 
-
+  resources :technologies, only: [:index]
 
   # search routes
   post 'search', to: 'search#index', as: 'search'
-  post 'search/suggestions', to: 'search#suggestions', as: 'search_suggestions'
+  get 'search_technologies', to: 'search#technologies', as: 'search_technologies'
+  get 'main_search_technologies', to: 'search#serp_page_technologies', as: 'main_search_technologies'
+
+  # tags
+  get 'tags_suggestions', to: 'tags#suggestions', as: 'tags_suggestions'
+
+  # post 'search/suggestions', to: 'search#suggestions', as: 'search_suggestions'
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root "static_pages#main_page"
+
+  # resources :dashboards
+  # get 'dashboard', to: 'dashboards#show', as: 'dashboard'
+
+
+
+  # should be last in list of all routes
+  get "/:username(/:target_folder)", to: "dashboards#show", as: 'dashboard'
+
+
+
 end
