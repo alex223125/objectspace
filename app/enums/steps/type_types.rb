afc808bbdb21b7384b2060fedef80f414cb24cf3
Alexey Sologub <alexeysologub51@gmail.com>
2023-07-08 21:21:09 -0400

    Enums: new step type added

---
+++ app/enums/steps/type_types.rb
@@ -0,0 +1,8 @@
+module Steps
+  class TypeTypes < ActiveEnum::Base
+    value :id => 1, :name => :regular
+    value :id => 2, :name => :wrapper
+    value :id => 3, :name => :container
+  end
+end
+
