af3050e0ec8f316d1b33f45aba32a0902e841f48
Alexey Sologub <alexeysologub51@gmail.com>
2023-07-08 21:04:35 -0400

    folder controller to add new child folders added + load of technolgies

---
+++ app/javascript/controllers/folder/folder_items_controller.js
@@ -0,0 +1,117 @@
+import { Controller } from '@hotwired/stimulus'
+// in use?
+
+export default class extends Controller {
+    // static targets = ["entries", "pagination"]
+    static targets = ['entries', 'newFolderAdditionButton']
+
+
+    initialize() {
+        console.log("Folder items controller initialized")
+
+        this.currentFolderId = ""
+        this.initialFolderId = ''
+
+        this.setInitialFolder()
+        this.setPathForNewFolderAdditionButton()
+
+        this.initialLoad()
+        // let options = {
+        //     rootMargin: '200px',
+        // }
+        //
+        // this.intersectionObserver = new IntersectionObserver(entries => this.processIntersectionEntries(entries), options)
+    }
+
+    connect() {
+        console.log("Folder items controller connected")
+        // this.username = ''
+    }
+
+
+    disconnect() {
+        console.log("Folder items controller disconnected")
+    }
+
+    // PRIVATE
+
+    setInitialFolder(){
+        // let queryString = window.location.search;
+        // let urlParams = new URLSearchParams(queryString);
+        // this.initialFolderId = urlParams.get('current_folder')
+        // ['http:', '', '127.0.0.1:3000', 'username-sdfsdf-33-aaaa', '12']
+        // http://127.0.0.1:3000/username-sdfsdf-33-aaaa/folder/12
+        this.initialFolderId = window.location.href.split('/')[5]
+        // this.username = window.location.href.split('/')[3]
+    }
+
+    initialLoad() {
+        console.log("initialLoad triggered")
+        // let url = `/${this.username}/folder/${this.initialFolderId}`
+
+        let url = ''
+        url = `/search_technologies?target_folder=${this.initialFolderId}`
+        // if (this.initialFolderId != '' && this.initialFolderId != null) {
+        //     console.log("11111")
+        //     url = `/search_technologies?target_folder=${this.initialFolderId}`
+        // } else {
+        //     console.log("22222")
+        //     url = '/technologies'
+        // }
+
+
+        Rails.ajax({
+            type: 'GET',
+            url: url,
+            dataType: 'json',
+            success: (data) => {
+                console.log(data)
+                this.insertFoldersAndTechnologies(data)
+
+                // this.setCurrentFolder(data.current_folder_id)
+                // this.setPathForNewFolderAdditionButton()
+                // this.entriesTarget.insertAdjacentHTML('beforeend', data.entries)
+            }
+        })
+    }
+
+    insertFoldersAndTechnologies(data) {
+        this.entriesTarget.insertAdjacentHTML('beforeend', data.entries)
+    }
+
+    // setCurrentFolder(folderId) {
+    //     this.currentFolderId = folderId
+    // }
+
+    setPathForNewFolderAdditionButton(){
+        let path =`/folders/new?target_folder=${this.initialFolderId}`
+        this.newFolderAdditionButtonTarget.href = path
+    }
+
+    // processIntersectionEntries(entries) {
+    //     entries.forEach(entry => {
+    //         if (entry.isIntersecting) {
+    //             this.loadMore()
+    //         }
+    //     })
+    // }
+
+    // loadMore() {
+    //     let next_page = this.paginationTarget.querySelector("a[rel='next']")
+    //     if (next_page == null) { return }
+    //     let url = next_page.href
+    //
+    //     Rails.ajax({
+    //         type: 'GET',
+    //         url: url,
+    //         dataType: 'json',
+    //         success: (data) => {
+    //             this.entriesTarget.insertAdjacentHTML('beforeend', data.entries)
+    //             this.paginationTarget.innerHTML = data.pagination
+    //         }
+    //     })
+    // }
+
+
+
+}
