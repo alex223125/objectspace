0a3a04454fb0aec84533cceb70081a40dd65b70e
Alexey Sologub <alexeysologub51@gmail.com>
2023-07-08 21:10:45 -0400

    USer entity: new validation added, new fields added, assications rebuilded

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ApplicationRecord
+  include Ownerable
+
+
+  USERNAME_MAXIMUM_LENGTH = 40.freeze
+
+  searchkick callbacks: :async,
+             text_middle: [:name, :username]
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -9,15 +17,24 @@ class User < ApplicationRecord
          # for Google OmniAuth
         :omniauthable, omniauth_providers: [:google_oauth2]
 
+  has_one :dashboard
+  # has_many :articles, class_name: "Articles::Article"
   has_many :folders
 
-  # validates :name, :username, :email, :presence => true
+  has_one_attached :avatar
+  has_one_attached :cropped_avatar
+
   validates :name, presence: true, allow_blank: false
-  validates :username, presence: true, allow_blank: false
   validates :email, presence: true, allow_blank: false
 
+  #username
+  validates :username, presence:true, allow_blank: false, length: { maximum: USERNAME_MAXIMUM_LENGTH }
   validates :username, :email, uniqueness: true
+  validates_with UsernameFormatValidator
+
   validates_acceptance_of :tos_agreement, allow_nil: false, on: :create, :message => "Terms and privacy policy must be accepted."
+
+  attr_reader :cropped_image_result
 
   def tos_agreement_signed?
     self.tos_agreement == true
@@ -30,7 +47,7 @@ class User < ApplicationRecord
 
   def self.from_omniauth(auth)
     binding.pry
-    user = User.where(provider: auth.provider, uid: auth.uid).first
+    user = Users::User.where(provider: auth.provider, uid: auth.uid).first
     if user.present?
       user
       # do nothing
