d6e1f47cc6c1fb6d38aaf5a1fd1fbba2d52636f7
Alexey Sologub <alexeysologub51@gmail.com>
2023-09-17 19:32:29 -0400

    concern for params for nested nodes during algorithm creation added

---
+++ app/controllers/algorithm/concerns/subnodable.rb
@@ -0,0 +1,37 @@
+module Algorithm
+  module Concerns
+    module Subnodable
+      extend ActiveSupport::Concern
+
+      def recursive_nested_substeps_attr
+        build_recursive_params(
+          recursive_key: 'subnodes_attributes',
+          parameters: @params,
+          permitted_attributes: [:technologiable_type,
+                                 :technologiable_id,
+
+                                 :id,
+
+                                 :position,
+                                 :type,
+                                 :title,
+                                 :instruction,
+                                 :step_finish_check,
+                                 :solves_the_problem,
+                                 :sources,
+                                 :additional_information,
+
+                                 :step_functional_type,
+                                 :control_structure_functional_type,
+                                 :note,
+                                 :description,
+
+                                 :_destroy,
+
+                                 conditions_attributes: [:id, :title, :instruction]]
+        )
+      end
+
+    end
+  end
+end
