b5c95e3fcdfa027b7847a599543adc6b90f9a782
Alexey Sologub <alexeysologub51@gmail.com>
2023-09-17 19:49:07 -0400

    helper for stimulus js remote_select_instruction controller added

---
+++ app/helpers/remote_select_instruction_helper.rb
@@ -0,0 +1,27 @@
+module RemoteSelectInstructionHelper
+
+  WRAPPER_STEP_ADDITION_FOR_REGULAR_CASE = "algorithm_form_wrapper_step_addition".freeze
+  WRAPPER_STEP_ADDITION_FOR_CLASS_LEVEL_ALGORITHM = "algorithm_form_class_level_wrapper_step_addition".freeze
+
+  REGULAR_FUNCT_TYPE = "regular".freeze
+  CLASS_LEVEL_FUNCT_TYPE = "class_level".freeze
+
+  def remote_select_instruction_select_type(key)
+    if functional_type(key) == REGULAR_FUNCT_TYPE
+      WRAPPER_STEP_ADDITION_FOR_REGULAR_CASE
+    elsif functional_type(key) == CLASS_LEVEL_FUNCT_TYPE
+      WRAPPER_STEP_ADDITION_FOR_CLASS_LEVEL_ALGORITHM
+    end
+  end
+
+  def regular_functional_type?(key)
+    Algorithms::FunctionalTypes[key] == REGULAR_FUNCT_TYPE || @functional_type == nil
+  end
+
+  private
+
+  def functional_type(key)
+    Algorithms::FunctionalTypes[key]
+  end
+
+end
