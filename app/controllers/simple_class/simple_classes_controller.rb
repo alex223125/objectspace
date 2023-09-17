db73537bbe993417221d5b6194dab0588833231f
Alexey Sologub <alexeysologub51@gmail.com>
2023-09-17 19:46:09 -0400

    endpoints for tree view added treejs

--- app/controllers/simple_class/simple_classes_controller.rb
+++ app/controllers/simple_class/simple_classes_controller.rb
@@ -2,7 +2,7 @@ class SimpleClass::SimpleClassesController < ApplicationController
   include Folderable
   include TechBreadcrumbable
 
-  before_action :set_simple_class, only: %i[ show edit update destroy preview ]
+  before_action :set_simple_class, only: %i[ show edit update destroy preview]
   before_action :set_target_folder, only: %i[ new create ]
 
   # GET /simple_classes or /simple_classes.json
@@ -27,20 +27,71 @@ class SimpleClass::SimpleClassesController < ApplicationController
     end
   end
 
+  def tree_view
+    binding.pry
+    @simple_class = SimpleClasses::SimpleClass.find(params[:id])
+
+    hash_tree = @simple_class.root_interface_group.hash_tree
+    root_interface_group = hash_tree.keys[0]
+
+    @root_actions = root_interface_group.interface_members
+    @children_interface_groups = root_interface_group.children
+
+    path = "simple_class/simple_classes/partials/tree_view/main"
+
+    binding.pry
+    respond_to do |format|
+      format.json {
+        render json: { tree_view: render_to_string(partial: path,
+                                                 formats: [:html],
+                                                 locals: { root_actions: @root_actions,
+                                                           children_interface_groups: @children_interface_groups })}
+      }
+    end
+
+  end
+
+  def tree_map
+    # TODO: create query object with 1 query with jons, current solution makes 4 queries
+    @simple_class = SimpleClasses::SimpleClass.where(uuid: params[:id])
+                                              .includes(:simple_class_attributes, :interface_groups)
+                                              .includes(interface_groups: [interface_members: [:memberable]]).first
+
+    if params[:map_type] == "technology_pick"
+      locals = { simple_class: @simple_class, type: "technology_pick"}
+    else
+      locals = { simple_class: @simple_class, type: "regular_view"}
+    end
+
+    path = "simple_class/simple_classes/partials/dynamic_tree_view/main"
+
+
+    respond_to do |format|
+      format.json {
+        render json: { tree: render_to_string(partial: path,
+                                                   formats: [:html],
+                                                   locals: locals)}
+      }
+    end
+  end
+
   # GET /simple_classes/new
   def new
     binding.pry
     if params[:class_type] == "decision_process_object_class"
-      @class_type = SimpleClasses::TypeTypes[:decision_process_object_class]
+      @class_type = SimpleClasses::FunctionalTypes[:decision_process_object_class]
     elsif params[:class_type] == "decision_object_class"
-      @class_type = SimpleClasses::TypeTypes[:decision_object_class]
+      @class_type = SimpleClasses::FunctionalTypes[:decision_object_class]
     elsif params[:class_type] == "decision_object_container_class"
-      @class_type = SimpleClasses::TypeTypes[:decision_object_container_class]
+      @class_type = SimpleClasses::FunctionalTypes[:decision_object_container_class]
     end
 
     @simple_class = SimpleClasses::SimpleClass.new
-    @root_class_container = @simple_class.class_containers.new(title: "Root class container")
-    @root_interface_group = @simple_class.interface_groups.new(title: "Root actions group")
+
+    @root_class_container = @simple_class.class_containers.new(title: "Main")
+
+    rig_description = "This is main interface group for all uncategorized actions."
+    @root_interface_group = @simple_class.interface_groups.new(title: "Main", description: rig_description)
 
     binding.pry
     service = Services::SimpleClasses::InterfaceGroups::CreateDefaultCollection.new(:decision_process_object_class)
@@ -53,6 +104,8 @@ class SimpleClass::SimpleClassesController < ApplicationController
 
   # GET /simple_classes/1/edit
   def edit
+    binding.pry
+    @root_interface_group = @simple_class.root_interface_group
   end
 
   # POST /simple_classes or /simple_classes.json
@@ -77,13 +130,19 @@ class SimpleClass::SimpleClassesController < ApplicationController
 
   # PATCH/PUT /simple_classes/1 or /simple_classes/1.json
   def update
+    binding.pry
+    service = Services::SimpleClasses::SimpleClasses::Update.new(simple_class_params, @simple_class)
+    service.call
+
     respond_to do |format|
-      if @simple_class.update(simple_class_params)
-        format.html { redirect_to simple_class_url(@simple_class), notice: "Simple class was successfully updated." }
+      if service.errors.blank?
+        binding.pry
+        format.html { redirect_to simple_class_path(username: service.simple_class.owner.ownername, id: service.simple_class.slug),
+                                  notice: "Simple class was successfully updated." }
         format.json { render :show, status: :ok, location: @simple_class }
       else
         format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @simple_class.errors, status: :unprocessable_entity }
+        format.json { render json: service.simple_class.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -117,9 +176,10 @@ class SimpleClass::SimpleClassesController < ApplicationController
     # Only allow a list of trusted parameters through.
     def simple_class_params
       params.require(:simple_classes_simple_class).permit(
-        :title, :description, :type,
+        :title, :description, :functional_type, :source_page_description,
         :instructionable_type, :instructionable_id, :tag_list,
-        interface_groups_attributes, class_containers_attributes
+        interface_groups_attributes, class_containers_attributes,
+        simple_class_attributes_attributes: [:id, :title, :description, :article_id, :_destroy ]
       )
     end
 
