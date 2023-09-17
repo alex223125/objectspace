f25e81ff07aefca94f228ec4ee8e18f905f298ae
Alexey Sologub <alexeysologub51@gmail.com>
2023-09-17 19:53:54 -0400

    dynamic step form controller updated

--- app/javascript/controllers/algorithms/dynamic_step_form_controller.js
+++ app/javascript/controllers/algorithms/dynamic_step_form_controller.js
@@ -1,32 +1,37 @@
+// TODO: rename to dynamic_node_form
 import { Controller } from '@hotwired/stimulus';
 
 export default class extends Controller {
-    static targets = ["dynamicStepForm", "parentOfDynamicStep", "currentStepFrontendId" ]
+    static targets = [
+        "dynamicNodeForm",
+        "parentOfDynamicNode",
+        "currentNodeFrontendId" ]
 
     initialize() {
-        console.log("Dynamic step controller initialized")
-        this.setParentStepIdValue()
-        this.setCurrentStepFronendIdValue()
+        console.log("Dynamic node controller initialized")
+        this.setParentNodeIdValue()
+        this.setCurrentNodeFronendIdValue()
     }
 
     connect() {
-        console.log("Dynamic step controller connected")
+        console.log("Dynamic node controller connected")
     }
 
     disconnect() {
-        console.log("Dynamic step controller disconnected")
+        console.log("Dynamic node controller disconnected")
     }
 
     // PRIVATE METHODS
-    setParentStepIdValue(){
+    setParentNodeIdValue(){
 
         // Here we go from up to bottom of the form
         // get parent instance key
-        var parentForm = this.dynamicStepFormTarget.closest('.nested-fields');
+        var parentForm = this.dynamicNodeFormTarget.closest('.nested-fields');
         console.log("parentForm")
         console.log(parentForm)
 
-        var firstInputOfParentForm = parentForm.querySelector('.step-position-hidden-field')
+        // TODO: use target for step-position-hidden-field
+        var firstInputOfParentForm = parentForm.querySelector('.node-position-hidden-field')
         console.log("firstInputOfParentForm")
         console.log(firstInputOfParentForm)
 
@@ -42,9 +47,9 @@ export default class extends Controller {
             console.log(instanceKey)
 
             // set this key to hidden field for usage
-            this.parentOfDynamicStepTarget.value = `dynamic_step_${instanceKey}`
-            console.log("this.parentOfDynamicStepTarget.value")
-            console.log(this.parentOfDynamicStepTarget.value)
+            this.parentOfDynamicNodeTarget.value = `dynamic_node_${instanceKey}`
+            console.log("this.parentOfDynamicNodeTarget.value")
+            console.log(this.parentOfDynamicNodeTarget.value)
         } else {
             // 2. When parent normally generated substep
             // a[0].querySelector('.substep-position-hidden-field').id.split("_").at(-2)
@@ -53,17 +58,17 @@ export default class extends Controller {
             console.log(instanceKey)
 
             // set this key to hidden field for usage
-            this.parentOfDynamicStepTarget.value = instanceKey
-            console.log("this.parentOfDynamicStepTarget.value")
-            console.log(this.parentOfDynamicStepTarget.value)
+            this.parentOfDynamicNodeTarget.value = instanceKey
+            console.log("this.parentOfDynamicNodeTarget.value")
+            console.log(this.parentOfDynamicNodeTarget.value)
         }
     }
 
-    setCurrentStepFronendIdValue(){
+    setCurrentNodeFronendIdValue(){
         // Here logic goes from generated input itslef
         // during iteration we create dynamic id with key
 
-        var firstInputId = this.currentStepFrontendIdTarget.id
+        var firstInputId = this.currentNodeFrontendIdTarget.id
         console.log("firstInputId")
         console.log(firstInputId)
 
@@ -85,7 +90,7 @@ export default class extends Controller {
             console.log(firstInputInstaceKey)
         }
 
-        this.currentStepFrontendIdTarget.value = `dynamic_step_${firstInputInstaceKey}`
+        this.currentNodeFrontendIdTarget.value = `dynamic_node_${firstInputInstaceKey}`
     }
 
 
