3ec55202b7e6e3180f0f9308ffbfe3be7a6a6332
Alexey Sologub <alexeysologub51@gmail.com>
2023-09-17 20:31:04 -0400

    Sortable controller for interface members to order their position for better readability

---
+++ app/javascript/controllers/interface_members/interface_members_drag_controller.js
@@ -0,0 +1,107 @@
+import { Controller } from "@hotwired/stimulus"
+import Sortable from "sortablejs"
+import {useMutation} from "stimulus-use";
+
+const nestedFieldsTemplateSelector = ".nested-fields-interface-members"
+
+export default class extends Controller {
+
+    static targets = [
+        'interfaceMembersArea',
+        'memberPositionVisibleField',
+        'memberPositionHiddenField'
+    ];A
+
+    connect() {
+        console.log("Interface members drag controller connected")
+        this.sortable = Sortable.create(this.element, {
+            group: 'interfaceMebmers',
+            animation: 150,
+            onEnd: this.end.bind(this)
+            // filter: ".nodraggablesteps"
+        })
+
+        // mutation observer to recalculate indexes when new step is added
+        this.membersAmount = this.currentMembersAmount()
+        useMutation(this, {attributes: false, childList: true, characterData: false, subtree:true})
+
+        this.handleFormWithPredefinedMembers()
+    }
+
+    disconnect() {
+        console.log("Interface members drag controller disconnected")
+    }
+
+    mutate(entries) {
+
+        var membersAmountAfterMutation = this.currentMembersAmount()
+        var change = this.membersAmount - membersAmountAfterMutation
+
+        if (change == 1)  {
+            var typeOfChange = 'memberRemoved'
+        } else if (change == -1) {
+            var typeOfChange = 'memberAdded'
+        }
+
+        if (typeOfChange == 'memberAdded') {
+            // new group added
+            this.recalculateIndexes()
+            this.membersAmount = this.membersAmount + 1
+        } else if (typeOfChange == 'memberRemoved') {
+            // group been removed
+            this.recalculateIndexes()
+            this.membersAmount = this.membersAmount - 1
+        }
+    }
+
+
+    // PRIVATE
+
+    handleFormWithPredefinedMembers(){
+        if (this.memberPositionHiddenFieldTargets.length >= 1 ) {
+            this.recalculateIndexes()
+        }
+    }
+
+    currentMembersAmount(){
+        return this.memberPositionHiddenFieldTargets.length
+    }
+
+
+    end(event) {
+        this.recalculateIndexes()
+    }
+
+    recalculateIndexes(){
+        // // recount hidden inputs indexes
+
+        // 1.select only visible (hidden steps - deleted steps taken from db to edit form)
+        let filteredHiddenFieldTargets = this.memberPositionHiddenFieldTargets.filter((field) => {
+            return field.closest(nestedFieldsTemplateSelector).style.display != 'none'
+        });
+
+        // 2.update indexes
+        // var positionHiddenElemenets = [...this.interfaceGroupsAreaTarget.querySelectorAll(".interface-group-position-hidden-field")]
+        var positionHiddenElemenets = [...filteredHiddenFieldTargets]
+        positionHiddenElemenets.forEach(function callback(value, index) {
+            value.value = (index + 1)
+        });
+
+
+        // 1.select only visible (hidden steps - deleted steps taken from db to edit form)
+        let filteredVisibleFieldTargets = this.memberPositionVisibleFieldTargets.filter((field) => {
+            return field.closest(nestedFieldsTemplateSelector).style.display != 'none'
+        });
+
+        // var positionHiddenElemenets = [...this.stepsAreaTarget.querySelectorAll(".step-position-visible-field")]
+        // var positionHiddenElemenets = [...this.interfaceGroupsAreaTarget.querySelectorAll(".interface-group-position-visible-field")]
+        var positionVisibleElemenets = [...filteredVisibleFieldTargets]
+        positionVisibleElemenets.forEach(function callback(value, index) {
+            value.innerHTML = (index + 1)
+            // value.text(index + 1)
+        });
+
+
+    }
+
+}
