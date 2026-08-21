fa04f1348910481c43458c8ef4cab60998990761
Alexey Sologub <alexdevindeva@gmail.com>
2026-08-21 06:21:06 +0300

    stmp config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,6 +11,18 @@ Rails.application.configure do
     protocol: "https"
   }
 
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "www.acty.wiki",
+    user_name: ENV["SMTP_USERNAME"],
+    password: ENV["SMTP_PASSWORD"],
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
+
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
